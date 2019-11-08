0c39fed4d9e87c5b518cf58cba9fea4341e70b42
Aleff Moura <aleffmds@gmail.com>
2019-11-08 20:28:04 -0200

    adicionado os migrations

---
+++ Api/Totten.Solutions.WolfMonitor/Totten.Solutions.WolfMonitor.Infra.ORM/Migrations/20191108222656_FirstMigration-Create-WolfMonitor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Totten.Solutions.WolfMonitor.Infra.ORM.Migrations
+{
+    public partial class FirstMigrationCreateWolfMonitor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Removed = table.Column<bool>(nullable: false),
+                    CreatedIn = table.Column<DateTime>(nullable: false),
+                    UpdatedIn = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    FantasyName = table.Column<string>(nullable: false),
+                    Cnpj = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Agents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Removed = table.Column<bool>(nullable: false),
+                    CreatedIn = table.Column<DateTime>(nullable: false),
+                    UpdatedIn = table.Column<DateTime>(nullable: false),
+                    CompanyId = table.Column<Guid>(nullable: false),
+                    UserWhoCreatedId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    LocalIp = table.Column<string>(nullable: true),
+                    HostName = table.Column<string>(nullable: true),
+                    HostAddress = table.Column<string>(nullable: true),
+                    Login = table.Column<string>(maxLength: 100, nullable: false),
+                    Password = table.Column<string>(maxLength: 100, nullable: false),
+                    Configured = table.Column<bool>(nullable: false),
+                    FirstConnection = table.Column<DateTime>(nullable: true),
+                    LastConnection = table.Column<DateTime>(nullable: true),
+                    LastUpload = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Agents_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Cnpj", "CreatedIn", "FantasyName", "Name", "Removed", "UpdatedIn" },
+                values: new object[] { new Guid("c576cf93-370c-4464-21f9-08d763d27d75"), "35.344.681/0001-90", new DateTime(2019, 11, 8, 20, 26, 55, 547, DateTimeKind.Local).AddTicks(2676), "TOTEM SOLUTIONS", "ALEFF MOURA DA SILVA 10685805425", false, new DateTime(2019, 11, 8, 20, 26, 55, 549, DateTimeKind.Local).AddTicks(9003) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agents_CompanyId",
+                table: "Agents",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agents_UserWhoCreatedId",
+                table: "Agents",
+                column: "UserWhoCreatedId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agents");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
---
+++ Api/Totten.Solutions.WolfMonitor/Totten.Solutions.WolfMonitor.Infra.ORM/Migrations/Auth/20191108222738_FirstMigration-Create-AuthContext.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Totten.Solutions.WolfMonitor.Infra.ORM.Migrations.Auth
+{
+    public partial class FirstMigrationCreateAuthContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Removed = table.Column<bool>(nullable: false),
+                    CreatedIn = table.Column<DateTime>(nullable: false),
+                    UpdatedIn = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Level = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Removed = table.Column<bool>(nullable: false),
+                    CreatedIn = table.Column<DateTime>(nullable: false),
+                    UpdatedIn = table.Column<DateTime>(nullable: false),
+                    CompanyId = table.Column<Guid>(nullable: false),
+                    RoleId = table.Column<Guid>(nullable: false),
+                    Login = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Cpf = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: true),
+                    Language = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "CreatedIn", "Level", "Name", "Removed", "UpdatedIn" },
+                values: new object[,]
+                {
+                    { new Guid("020894b7-53f1-468a-9695-a2b6c47406ce"), new DateTime(2019, 11, 8, 20, 27, 37, 456, DateTimeKind.Local).AddTicks(6569), 0, "Agent", false, new DateTime(2019, 11, 8, 20, 27, 37, 463, DateTimeKind.Local).AddTicks(590) },
+                    { new Guid("4ef84fad-8dee-4395-bae1-65fa8fa11dcc"), new DateTime(2019, 11, 8, 20, 27, 37, 465, DateTimeKind.Local).AddTicks(9185), 1, "User", false, new DateTime(2019, 11, 8, 20, 27, 37, 465, DateTimeKind.Local).AddTicks(9229) },
+                    { new Guid("d300b446-2fd4-4059-a074-6af0ba419cea"), new DateTime(2019, 11, 8, 20, 27, 37, 465, DateTimeKind.Local).AddTicks(9410), 2, "Administrador", false, new DateTime(2019, 11, 8, 20, 27, 37, 465, DateTimeKind.Local).AddTicks(9429) },
+                    { new Guid("05f62ca8-462f-4796-bab1-ed083cebf096"), new DateTime(2019, 11, 8, 20, 27, 37, 465, DateTimeKind.Local).AddTicks(9478), 2, "System", false, new DateTime(2019, 11, 8, 20, 27, 37, 465, DateTimeKind.Local).AddTicks(9498) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CompanyId", "Cpf", "CreatedIn", "Email", "FirstName", "Language", "LastName", "Login", "Password", "Removed", "RoleId", "UpdatedIn" },
+                values: new object[] { new Guid("e0704212-13e5-4809-9b45-5d1f222eceec"), new Guid("c576cf93-370c-4464-21f9-08d763d27d75"), "10685805425", new DateTime(2019, 11, 8, 20, 27, 37, 492, DateTimeKind.Local).AddTicks(7594), "aleffmds@gmail.com", "Aleff", "pt-BR", "Moura da Silva", "alefmoura", "YWLA/fjq/N2i5CmP6+HJMXDslpQwLLP2tHl4E9NaM8w=", false, new Guid("05f62ca8-462f-4796-bab1-ed083cebf096"), new DateTime(2019, 11, 8, 20, 27, 37, 492, DateTimeKind.Local).AddTicks(7663) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CompanyId",
+                table: "Users",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
