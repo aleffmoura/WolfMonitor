f57fdef10d319294e876a897aaf919baf94af6ad
Aleff Moura <aleffmds@gmail.com>
2019-11-16 17:01:43 -0300

    adicionado novo projeto ao qual será o client de serviço windows

---
+++ Clients/Totten.Solutions.WolfMonitor.App/Totten.Solutions.WolfMonitor.Client.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Totten.Solutions.WolfMonitor.Client.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
