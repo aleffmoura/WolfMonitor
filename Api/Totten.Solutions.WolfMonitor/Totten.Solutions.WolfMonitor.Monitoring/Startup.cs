06a30a40c87d8058ef6a0505c0fc77fd869a5828
Aleff Moura <aleffmds@gmail.com>
2019-11-25 13:14:34 -0300

    modificado program e startup de monitoring

--- Api/Totten.Solutions.WolfMonitor/Totten.Solutions.WolfMonitor.Monitoring/Startup.cs
+++ Api/Totten.Solutions.WolfMonitor/Totten.Solutions.WolfMonitor.Monitoring/Startup.cs
@@ -1,27 +1,26 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SimpleInjector;
+using Totten.Solutions.WolfMonitor.Cfg.Startup;
 
 namespace Totten.Solutions.WolfMonitor.Monitoring
 {
     public class Startup
     {
+        private readonly Container container = new Container();
         public void ConfigureServices(IServiceCollection services)
         {
+            services.DefaultServiceSetup(container);
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app,
+                              IApplicationLifetime lifetime,
+                              ILoggerFactory loggerFactory,
+                              IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.DefaultApplicationSetup(lifetime, loggerFactory, env, container);
         }
     }
 }
